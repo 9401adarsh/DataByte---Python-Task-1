2e6f22797116c40cd0e99f5d802bfc6aa1c27e6a
Adarsh Ashokan <64418474+9401adarsh@users.noreply.github.com>
2021-01-04 13:40:02 +0530

    Update Python-Qn1.py

--- Python-Qn1.py
+++ Python-Qn1.py
@@ -8,9 +8,9 @@ Created on Sun Jan  3 10:19:18 2021
 for i in range(1, 101):
     if(i % 15 == 0):
         print("FizzBuzz")
-    elif(i% 5 == 0):
+    elif(i% 3 == 0):
         print("Fizz")
-    elif(i % 3 == 0):
+    elif(i % 5 == 0):
         print("Buzz")
     else:
         print(i)
